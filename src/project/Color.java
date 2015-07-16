5a116a05a582ffd0a48b6f6c784e4ed6b09d557c
avneetkaur <avneetkaurgadh@gmail.com>
2015-07-15 22:35:51 -0400

    created color class
    This class is used to show the color of lantern cards, lake tiles for playing the game.

---
+++ src/project/Color.java
@@ -0,0 +1,35 @@
+package project;
+
+import java.util.*;
+
+/**
+ * This class is used to show the color of lantern cards, lake tiles for playing the game
+ * @author Avneet
+ * @version 1.0
+ */
+
+public enum Color {
+	ORANGE, GREEN, PURPLE, WHITE, BLUE, RED, BLACK;
+	/**
+	 * This list is used when created random color
+	 * with an unmodifiable view of the specific collection
+	 */
+	private static final List<Color> VALUES = Collections
+			.unmodifiableList(Arrays.asList(values()));
+	/**
+	 * size of color
+	 */
+	private static final int SIZE = VALUES.size();
+	/**
+	 * this object is used to random the color 
+	 */
+	private static final Random RANDOM = new Random();
+	
+	/**
+	 * random color of lake tiles
+	 * @return Color 
+	 */
+	public static Color randomColor() {
+		return VALUES.get(RANDOM.nextInt(SIZE));
+	}
+}
