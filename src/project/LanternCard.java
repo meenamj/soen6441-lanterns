55d82d7a6231e13eb61b7838c9068037d430c003
meenakshi <meena_jyo63@yahoo.in>
2015-07-15 21:49:53 -0400

    created lantern card and supply class
    In lantern card class the player can get the lantern card after placing
    lake tile and for supply class its used for seven lantern stacks
    creation

---
+++ src/project/LanternCard.java
@@ -0,0 +1,37 @@
+package project;
+
+import java.io.Serializable;
+/**
+ * Lantern card is a card which player can get after placed lake tile
+ * @author Meenakshi
+ */
+public class LanternCard implements Serializable{
+	/**
+	 * color of lantern card
+	 */
+	private Color c;
+	/**
+	 * Constructor of lantern card
+	 * @param c color of lantern card
+	 */
+	public LanternCard(Color c){
+		setColor(c);
+	}
+	
+	/**
+	 * Set color of lantern card
+	 * @param c color of lantern card
+	 */
+	public void setColor(Color c){
+		this.c = c;
+	}
+	
+	/**
+	 * Get color of lantern card
+	 * @return lantern card
+	 */
+	public Color getColor(){
+		return c;
+	}
+	
+}
