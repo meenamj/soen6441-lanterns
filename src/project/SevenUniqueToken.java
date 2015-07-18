e8a8604a7e8266b8f28edf42ad5e77adbdc66de9
Adigun Jide Idris <adigun.idris@ymail.com>
2015-07-17 23:25:16 -0400

    correct author tag

--- src/project/SevenUniqueToken.java
+++ src/project/SevenUniqueToken.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
  * Seven unique token is one of dedication token
  * when players have seven lantern cards which have the all different color,
  * the player can exchange this card
- * * @author Idris
+ * @author Idris
  * @version 1.1
  */
 public class SevenUniqueToken extends DedicationToken {
