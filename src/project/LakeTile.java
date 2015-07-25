c579baffe515579d347adb016f8eb3a5895a942f
Nirav Bhut <nirav.bhut1210@gmail.com>
2015-07-24 23:41:38 -0400

    lake tiles position on board
    position of lake tiles on board can be set and get

--- src/project/LakeTile.java
+++ src/project/LakeTile.java
@@ -8,13 +8,13 @@ import java.util.Random;
 import java.util.Vector;
 
 /**
- * Lake tile is a card to place to the game
- * After that player can get lantern cards
- * following the direction of the placed lake tile.
- * @author 
+ * Lake tile is a card to place to the game After that player can get lantern
+ * cards following the direction of the placed lake tile.
+ * 
+ * @author
  */
-public class LakeTile implements Serializable{
-	
+public class LakeTile implements Serializable {
+
 	/**
 	 * index
 	 */
@@ -22,7 +22,7 @@ public class LakeTile implements Serializable{
 	/**
 	 * four colors of lake tile
 	 */
-private Queue<Color> colorOfFourSides;
+	private Queue<Color> colorOfFourSides;
 	/**
 	 * rotation of lake tile on board (4 type: 0, 90 , 180 , 270 degree)
 	 */
@@ -30,38 +30,25 @@ private Queue<Color> colorOfFourSides;
 	/**
 	 * if four sides on the lake tile is close to the other lake tile
 	 */
-	private boolean[] position;
+	private boolean[] isAvailablePosition;
 	/**
 	 * platform is used to get favor token
 	 */
 	private boolean hasPlatform;
-	/**
-	 * random is used to random the red color which is only used in the getRandomColorNotRed method 
-	 */
-	private Random random = new Random();
-
-	/**
-	 * Get random color which is not red
-	 * @return a color which is not red
-	 */
-	private Color getRandomColorNotRed() {
-		Color randomColor;
-		do {
-			randomColor = Color.random();
-		} while (randomColor == Color.RED);
-		return randomColor;
-	}
 	
-	/*
-	 * Constructor of a lake tile
-	 * Generate a lake tile with random color for each sides
+	/**
+	 * Constructor of a lake tile Generate a lake tile with random color for
+	 * each sides
+	 * 
 	 * @param true for this lake tiles is a start lake tile
 	 */
-	public LakeTile(int index, Color c1, Color c2, Color c3, Color c4, boolean platform) {
+	public LakeTile(int index, Color c1, Color c2, Color c3, Color c4,
+			boolean platform) {
 		Queue<Color> cos = new LinkedList<Color>();
 		this.index = index;
-		//start laketile
-		if(index == 27){
+		isAvailablePosition = new boolean[]{true,true,true,true};
+		// start laketile
+		if (index == 27) {
 			setRotation(Rotation.D270);
 		}
 		cos.add(c1);
@@ -71,9 +58,10 @@ private Queue<Color> colorOfFourSides;
 		this.colorOfFourSides = cos;
 		this.hasPlatform = platform;
 	}
-	
+
 	/*
 	 * Get four color of lake tiles
+	 * 
 	 * @return four colors of a lake tile
 	 */
 	public Queue<Color> getColorOfFourSides() {
@@ -82,6 +70,7 @@ private Queue<Color> colorOfFourSides;
 
 	/*
 	 * Set four color of lake tiles
+	 * 
 	 * @param four colors of a lake tile
 	 */
 	public void setColorOfFourSides(Queue<Color> colorOfFourSides) {
@@ -103,33 +92,32 @@ private Queue<Color> colorOfFourSides;
 	public void setIndex(int index) {
 		this.index = index;
 	}
-	
-	
-	//Build-2 
-	
+
+	// Build-2
+
+	/**
+	 * get all positions of a lake tiles, return values will be 'true' or
+	 * 'false'
+	 * 
+	 * @return boolean of four positions
+	 */
+	public boolean[] getAvailablePosition() {
+		return isAvailablePosition;
+	}
 	
 	/**
-	 * get all positions of a lake tiles, return values will be 'true' or 'false'
-	 * @return boolean of top position 
+	 * To get the rotation of a lake tile
+	 * @return rotation value
 	 */
-	public boolean getTopPosition(){
-		return position[0];
-	}
-	public boolean getLeftPosition(){
-		return position[1];
-	}
-	public boolean getBottomPosition(){
-		return position[2];
-	}
-	public boolean getRightPosition(){
-		return position[3];
-	}
-
 	public Rotation getRotation() {
 		return rotation;
 	}
 
+	/**
+	 * To rotate a lake tile at some angle
+	 * @param rotation angle of rotation
+	 */
 	public void setRotation(Rotation rotation) {
 		this.rotation = rotation;
-	}	
+	}
 }
