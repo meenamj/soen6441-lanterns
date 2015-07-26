dc89f81b60098cd2b5ef51c62a16b4b7c09fe01d
Adigun Jide Idris <adigun.idris@ymail.com>
2015-07-26 14:52:30 -0400

    Remove unclear comments

--- src/project/Game.java
+++ src/project/Game.java
@@ -459,7 +459,7 @@ public class Game implements Serializable {
 				break;
 
 			case 2:
-<<<<<<< HEAD
+
 				System.out.println("What type of dedication do you want to make? ");
 				System.out.println(" 0. Four of A Kind");
 				System.out.println(" 1. Three Pair");
@@ -467,7 +467,6 @@ public class Game implements Serializable {
 				do {
 					choice = inputscan.next();
 				} while (!choice.equals("0") && !choice.equals("1") && !choice.equals("2"));
-=======
 				System.out
 						.println("What type of dedication do you want to make? ");
 				System.out.println(" 1. Four of A Kind");
@@ -477,7 +476,6 @@ public class Game implements Serializable {
 					choice = inputscan.next();
 				} while (!choice.equals("0") && !choice.equals("1")
 						&& !choice.equals("2") && !choice.equals("3"));
->>>>>>> origin/master
 				int choiceI = Integer.parseInt(choice);
 				current_player.makeDedication(choiceI);
 				break;
