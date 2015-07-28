42469e916bccdee7c8407bca61cd25fc92b1b7a4
meenamj <meena_jyo63@yahoo.in>
2015-07-28 14:26:49 -0400

    added new test method

--- src/testing/GameTest.java
+++ src/testing/GameTest.java
@@ -27,5 +27,17 @@ public class GameTest {
 		Game g = new Game("Player1","Player2","Player3","Player4");
 		g.startGame();
 		assertEquals("not 4 Players in the game ", 4, g.getPlayers().size());
+		
+	@Test
+	public void testgetNumberOfLanternCardsOnHand() throws Exception {
+				
+		String[] playersNames = new String[] {"One", "Two", "Three"};
+		Game g = new Game(playersNames);
+		int actual = g.getNumberOfLanternCardsOnHand();
+		int expected = 1;
+		assertEquals(expected, actual);
+	
 	}
-}
+		
+	}
+
