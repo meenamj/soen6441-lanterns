c6cb9db812fddd8b5ac883b1487ca15843fba976
Nuttakit <nutphi@outlook.com>
2015-07-28 16:21:56 -0400

    Update AllTests.java

--- src/testing/AllTests.java
+++ src/testing/AllTests.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ GameFileTest.class, GameTest.class, LakeTileTest.class,
-		PlayAreaTest.class, SupplyTest.class })
+		PlayAreaTest.class, SupplyTest.class, PlayerTest.class })
 public class AllTests {
 
 }
