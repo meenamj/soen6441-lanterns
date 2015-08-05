9e2bc542d1c7083a0a9af6d99960d14de159215d
Nirav Bhut <nirav.bhut1210@gmail.com>
2015-08-04 22:28:26 -0400

    Update Strategy.java

--- src/project/strategy/Strategy.java
+++ src/project/strategy/Strategy.java
@@ -6,5 +6,5 @@ public interface Strategy extends Serializable{
 	public enum Name{
 		START, MAINMENU, CHOOSE_LANTERN, MAKE_DEDICATION, SELECT_LAKE, SELECT_BOARD_POSITION, SELECT_LAKE_ROTATION
 	}
-	public int inputOption(int number_options, Strategy.Name status);
+	public int inputOption(int number_options, Strategy.Name status, Game game);
 }
