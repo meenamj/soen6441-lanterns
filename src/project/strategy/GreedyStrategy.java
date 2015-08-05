175a85e16094592dfae5e236b3736a5e8c112f39
Nirav Bhut <nirav.bhut1210@gmail.com>
2015-08-04 22:22:08 -0400

    greedy strategy implementation started

--- src/project/strategy/GreedyStrategy.java
+++ src/project/strategy/GreedyStrategy.java
@@ -1,6 +1,12 @@
 package project.strategy;
 
+import java.util.Queue;
 import java.util.Scanner;
+
+import project.Game;
+import project.PlayArea;
+import project.Player;
+import project.Position;
 
 public abstract class GreedyStrategy implements Strategy{
 	/**
@@ -8,8 +14,10 @@ public abstract class GreedyStrategy implements Strategy{
 	 * @param n_option user input
 	 * @return integer value of the option selected
 	 */
-	public int inputOption(int number_options, Strategy.Name status)
+	public int inputOption(int number_options, Strategy.Name status, Game game)
 	{
+		Queue players = game.getPlayers();
+		PlayArea pa = game.getPlayArea();
 		Scanner inputscan = new Scanner(System.in);
 		String in = null;
 		boolean validation = false;
@@ -25,11 +33,24 @@ public abstract class GreedyStrategy implements Strategy{
 				if (in.equals("" + i)) 
 				{
 					validation = true;
+					if(status == Name.MAINMENU){
+						makeADedicationMenu(players);
+						try {
+							simulateGame( players,pa);
+						} catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
 				}
 			}
 		}
 		while (!validation);
 		return Integer.parseInt(in);
+	
 	}
+	
+	protected abstract void makeADedicationMenu(Queue players);
+	protected abstract Position simulateGame (Queue player, PlayArea playarea) throws Exception;
 
 }
