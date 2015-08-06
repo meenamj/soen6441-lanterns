de9a1fbee73f448f90d535c0f48a21619aa2bf7a
Nirav Bhut <nirav.bhut1210@gmail.com>
2015-08-06 16:05:50 -0400

    get all possible solutions
    iteration through all the possible options to put a lake tile on the board

--- src/project/strategy/Greed.java
+++ src/project/strategy/Greed.java
@@ -7,6 +7,8 @@ import java.util.Random;
 import java.util.Vector;
 
 import project.Color;
+import project.Game;
+import project.LakeTile;
 import project.PlayArea;
 import project.Player;
 import project.Position;
@@ -15,21 +17,23 @@ import project.Rotation;
 public class Greed extends GreedyStrategy{
 	int lakeTiletochoose = 0;
 	private static final Random RANDOM = new Random();
-	protected void makeADedicationMenu(Queue players){
+	
+	/**
+	 * Pick a lake tile with favor token from the players hand, if there is not
+	 * a lake tile with favor token on the hand, then select a random tile. 
+	 */
+	protected void PickALakeTile(Queue<Player> players){
 		
 		ArrayList<Player> playerList = new ArrayList<Player>(players);
 		Player player = playerList.get(0);
 		
-		System.out.println("player name issss: "+ player.getName());
 		int numberOfLanternCards = player.getLanternCards().size();
+		
 		if(numberOfLanternCards >0  && numberOfLanternCards<3)
 		{
-			System.out.println(player.getLakeTiles().size() +"number of lake tiles a player has");
 			for(int i=0; i<player.getLakeTiles().size(); i++){
 				boolean lakeTileWithPlatform = player.getLakeTiles().get(i).isPlatform();
-				//System.out.println("platfrom : "+ player.getLakeTiles().get(i).isPlatform());
 				if(lakeTileWithPlatform){
-					System.out.print("found it!");
 					lakeTiletochoose = i;
 					break;
 				}
@@ -42,13 +46,16 @@ public class Greed extends GreedyStrategy{
 		System.out.println("Player selected "+ lakeTiletochoose);
 	}
 	
-	protected Position simulateGame(Queue players, PlayArea playarea) throws Exception{
+	/**
+	 * 
+	 */
+	protected Position checkLakeTilewithAvailable(Queue<Player> players, PlayArea playarea) throws Exception{
 		ArrayList<Player> playerList = new ArrayList<Player>(players);
 		Player player = playerList.get(0);
-		Position selectedIndex = null;
-		Queue color = player.getLakeTiles().get(lakeTiletochoose).getColorOfFourSides();
+		Queue<Color> color = player.getLakeTiles().get(lakeTiletochoose).getColorOfFourSides();
 		ArrayList<Color> list = new ArrayList<Color>(color);
 		ArrayList<Position> availableList = playarea.getPositionAvailableLakeTileOnBoard();
+		Position selectedIndex = null;
 	
 		for(int j=0; j< availableList.size(); j++)
 		{
@@ -57,16 +64,14 @@ public class Greed extends GreedyStrategy{
 			{
 				HashMap<Rotation, Vector<Object>> color_platform = playarea.getAdjacentColor(index);
 				Color c = list.get(i);
-				//System.out.println(c.name());
-				//System.out.println("color_platformss : "+color_platform);
+
 				for(Rotation r : Rotation.values())
 				{
-					//System.out.println(color_platform.get(r).get(0));
 					if(color_platform.get(r) != null){
 						Color colors = (Color) color_platform.get(r).get(0);
 						if(c.name() == colors.name()){
 							System.out.println("color found : "+color_platform.get(r).get(0));
-							System.out.println("index selected :"+ j);
+							System.out.println("index selected :"+ j); 
 							selectedIndex = index;
 							break;
 						}
@@ -78,4 +83,74 @@ public class Greed extends GreedyStrategy{
 		return selectedIndex;
 	}
 	
+	/**
+	 * to check if the player can make a dedication or not for the
+	 * current situation
+	 */
+	protected boolean canMakeDedication(Player player){
+		boolean flag;
+		if(player.isFourOfAKind() || player.isThreePair() || player.isSevenUnique()){
+			System.out.println("Dedication is possible to make");
+			flag = true;
+		}
+		else{
+			flag=false;
+		}
+		return flag;
+	}
+	
+	/**
+	 * check all possible solutions for each three steps of place a lake tile
+	 * @throws Exception 
+	 */
+	protected void simulateGamePlay(Queue<Player> players, PlayArea playarea, Game game) throws Exception
+	{
+		
+		ArrayList<Player> playerList = new ArrayList<Player>(players);
+		Player player = playerList.get(0);
+		int lanterns = player.getLanternCards().size();
+		ArrayList<Position> availableList = playarea.getPositionAvailableLakeTileOnBoard();
+		
+		for(int i=0;i<player.getLakeTiles().size();i++)
+		{
+			LakeTile active_laketile = player.getLakeTiles().remove(i);
+			ArrayList<HashMap<Rotation, Vector<Object>>> adjacent_color_list = player.placeLakeTileMenu(playarea, active_laketile);
+
+			for(int j=0; j<availableList.size();j++)
+			{
+				System.out.println("positions : "+ availableList.get(j).getText());
+				int pos_laketile_opt = j;
+				HashMap<Rotation, Vector<Object>> adjacent_colors = player.getPossibleRotation(availableList, adjacent_color_list, playarea, active_laketile, pos_laketile_opt);
+
+				for(int k=0;k<4;k++)
+				{
+
+					int rotation_opt = k;
+					player.setRotationOnActiveLakeTile(active_laketile, rotation_opt);
+					
+					game.distributeLanternCard(active_laketile, playarea.getSupply());
+					game.getBonusPlaceLakeTile(active_laketile, adjacent_colors);
+					/*if(canMakeDedication(player)){
+						System.out.println("This is the perfect lake tile to put");
+						break;
+					}*/
+					
+					System.out.print("player name is: "+player.getName());
+					System.out.println("------ number of lanterns: "+lanterns);
+					lanterns = player.getLanternCards().size();
+					if(lanterns >10){
+						System.out.println("more than 10");
+						break;
+					}
+					int x = availableList.get(j).getX();
+					int y = availableList.get(j).getY();
+					game.getPlayArea().getLakeTilesOnBoard()[x][y] = null;
+				
+					//System.out.println();
+				}
+			}
+		}
+		System.out.println(playarea.getLakeTileBoardText());
+		
+	}	
 }
