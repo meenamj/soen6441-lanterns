27e096cc59a6a4197762fb208030c23ae7576045
Nirav Bhut <nirav.bhut1210@gmail.com>
2015-08-08 19:40:14 -0400

    updated to work with solutions for unfriendly

--- src/project/strategy/UnfriendlyStrategy.java
+++ src/project/strategy/UnfriendlyStrategy.java
@@ -1,37 +1,93 @@
 package project.strategy;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Queue;
 
+import project.Color;
 import project.Game;
+import project.Player;
+
 
 public abstract class UnfriendlyStrategy implements Strategy{
+	ArrayList<Integer> solution = new ArrayList<Integer>();
+	int[] ExchangeOptions = new int[2];
+	
 	/**
 	 * This method check the input the user provides
-	 * @param n_option user input
+	 * @param number_options available for the user input
+	 * @param status name of the option menu
+	 * @param game instance of the game
 	 * @return integer value of the option selected
 	 */
 	public int inputOption(int number_options, Strategy.Name status, Game game)
 	{
-		Scanner inputscan = new Scanner(System.in);
-		String in = null;
+		Game gameClone = game.clone();
+		
+		Queue<Player> players = gameClone.getPlayers();
+		ArrayList<Player> playerList = new ArrayList<Player>(players);
+        Player player = playerList.get(0);
+        
+		Color c ;
+		
+		int in = 0;
 		boolean validation = false;
 		do
 		{
-			if (in != null) 
-			{
-				System.out.println(in + " is not in the option");
-			}
-			in = inputscan.next();
 			for (int i = 0; i < number_options; i++)
 			{
-				if (in.equals("" + i)) 
+				if (in ==  i)
 				{
 					validation = true;
+					if(status == Name.MAINMENU){
+						
+						if(canMakeDedication(players)){
+							in = 2;
+						}
+						else if(canExchange(players, gameClone)){
+							ExchangeOptions = performExchange(player,gameClone);
+							in = 1;
+						}
+						else {
+							try {
+								
+								solution = simulateGamePlay(game);
+								in = 3;
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					}
+					else if(status == Name.CHOOSE_LANTERN_HAND){
+						in = ExchangeOptions[0];
+					}
+					else if(status == Name.CHOOSE_LANTERN_SUPPLY){
+						in = ExchangeOptions[1];
+					}
+					else if(status == Name.MAKE_DEDICATION){
+						int choice = whichDedication(players);
+						in = choice;
+					}
+					else if(status == Name.SELECT_LAKE){
+						in =  solution.get(0);
+					}
+					else if(status == Name.SELECT_BOARD_POSITION){
+						in = solution.get(1);
+					}
+					else if(status == Name.SELECT_LAKE_ROTATION){
+						in = solution.get(2);
+					}
 				}
 			}
 		}
 		while (!validation);
-		return Integer.parseInt(in);
+		System.out.println(in);
+		return in;
+	
 	}
-
+	
+	protected abstract ArrayList<Integer> simulateGamePlay(Game game) throws Exception;
+	protected abstract boolean canMakeDedication(Queue<Player> players);
+	protected abstract int whichDedication(Queue<Player> players);
+	protected abstract boolean canExchange(Queue<Player> players, Game game);
+	protected abstract int[] performExchange(Player player,Game game);
 }
