7eee9f5eac8aa892811f9cdd63e8964e5a7dc673
Avneetkaur <avneetkaurgadh@gmail.com>
2015-08-10 14:03:50 -0400

    recreated junit suite to run all test cases on AllTests.java

--- src/testing/AllTests.java
+++ src/testing/AllTests.java
@@ -5,9 +5,9 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ GameFileTest.class, GameTest.class, LakeTileTest.class,
-		PlayAreaTest.class, PlayerTest.class, PositionTest.class,
-		RotationTest.class, SupplyTest.class })
+@SuiteClasses({ DisasterTest.class, GameFileTest.class, GameTest.class,
+		LakeTileTest.class, PlayAreaTest.class, PlayerTest.class,
+		PositionTest.class, RotationTest.class, SupplyTest.class })
 public class AllTests {
 
 }
