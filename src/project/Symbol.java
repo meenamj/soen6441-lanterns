dfed5bce5fdb6c4fb76bfee571dc10a0789a3c5d
meenamj <meena_jyo63@yahoo.in>
2015-08-12 23:37:25 -0400

    added symbol javadoc and generated the document.

--- src/project/Symbol.java
+++ src/project/Symbol.java
@@ -3,11 +3,32 @@ package project;
  * This class is used for getting a string of unicode.
  */
 public class Symbol {
+	/**
+	 * ( &uarr; ) 
+	 */
 	public final static String UP = "\u2191";
+	/**
+	 * ( &rarr; )
+	 */
 	public final static String RIGHT = "\u2192";
+	/**
+	 * ( &darr; )
+	 */
 	public final static String DOWN = "\u2193";
+	/**
+	 * ( &larr; )
+	 */
 	public final static String LEFT = "\u2190";
+	/**
+	 * ( &#x273f; )
+	 */
 	public final static String PLATFORM = "\u273f";
+	/**
+	 * ( &#x2715; )
+	 */
 	public final static String NOT = "\u2715";
+	/**
+	 * ( &bull; )
+	 */
 	public final static String BULLET = "\u2022";
-}
+} 	
