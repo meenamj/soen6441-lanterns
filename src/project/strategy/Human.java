9f8136fed080f9474fc6d76c6f384542615622b6
Nirav Bhut <nirav.bhut1210@gmail.com>
2015-08-13 19:16:56 -0400

    updated a method that overrides the input options

--- src/project/strategy/Human.java
+++ src/project/strategy/Human.java
@@ -1,7 +1,10 @@
 package project.strategy;
+
+import java.util.Scanner;
+
 /**
  * This class is a concrete class for Human strategy player type.
- * @author Nuttakit
+ * @author None
  * @version 3.0
  */
 public class Human extends HumanStrategy{
@@ -10,5 +13,62 @@ public class Human extends HumanStrategy{
 	 * It is used to keep the correct version.
 	 */
 	private static final long serialVersionUID = -2966957739254565623L;
-
+	/**
+	 * This method is to get options players can select.
+	 * @param start_options start number of option which players can select.
+	 * @param end_options last number option which players can select.
+	 * @return The option selected by player.
+	 */
+	public int inputOption(int start_options, int end_options)
+	{
+		inputscan = new Scanner(System.in);
+		String in = null;
+		boolean validation = false;
+		do
+		{
+			if (in != null) 
+			{
+				System.out.println(in + " is not in the option");
+			}
+			in = inputscan.next();
+			for (int i = start_options; i <= end_options; i++)
+			{
+				if (in.equals("" + i)) 
+				{
+					validation = true;
+				}
+			}
+		}
+		while (!validation);
+		return Integer.parseInt(in);
+	}
+	/**
+	 * This method is to get options players can select.
+	 * @return true/false depends on questions.
+	 */
+	public boolean inputYesNo(){
+		inputscan = new Scanner(System.in);
+		String in = null;
+		do{
+			if(in !=null){
+				System.out.println(in+" is not the option");
+			}
+			in = inputscan.next();
+			in = in.toUpperCase();
+		}while(!in.equals("Y")&&!in.equals("N"));
+		if(in.equals("Y")){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	/**
+	 * This method is for the players to put input string.
+	 * @return text of input.
+	 */
+	public String inputString(){
+		inputscan = new Scanner(System.in);
+		return inputscan.next();
+	}
 }
